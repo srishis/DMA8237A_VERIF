445268c362d664910842ce5f36c1515de2812b64
vyawahare16 <vyawahareadiytya@gmail.com>
2020-05-25 20:05:06 -0700

    Sanity test

--- dma_configuration.sv
+++ dma_configuration.sv
@@ -1,3 +1,13 @@
+
+`define COMMAND_REG 4'h1000
+`define MODE_REG 4'h1011
+`define REQUEST_REG 4'h1001
+`define MASK_REG 4'h1010
+`define MASK_REG 4'h1111
+`define TEMPORARY_REG 4'h1101
+`define STATUS_REG 4'h1000
+
+
 class dma_configuration;
   // create all mailboxes required in the environment 
   static mailbox gen2drv;
--- dma_generator.sv
+++ dma_generator.sv
@@ -1,31 +1,72 @@
 class dma_generator;
 
-	dma_transaction tx;
+	dma_transaction tx,rx;
 	dma_transaction txQ[$];
+	
+	int reg_test,reg_pass,reg_fail;
 	
 	task run();
 	
 	//use of rand sequence for different tests
 	randsequence(main)
 	main: test_1;
-	test_1: dma_test;
+	test_1: basic_read_write_sanity_test;
 	endsequence
 		
 	endtask
 // test cases for design
 
-	// simple test for DMA with 10 random transactions
-		task dma_test;
-		repeat(10) begin
-		tx = new;
-		if(!tx.randomize()) $display("ERROR:DMA GENERATOR::RANDOMIZATION FAILED");
+	// simple basic_read_write_sanity_test for DMA with 
 		
+		
+		task basic_read_write_sanity_test;
+		//READING REGISTERS
+		for(int i = 0; i<`ADDRESS_LENGTH; i++)begin
+		tx = new();
+		if(!tx.randomize() with {tx.cs = 1'b0; tx.ior = 1'b0; tx.iow = 1'b1;  tx.addr_lo = i; tx.dreq = 4'b0; tx.hlda = 1'b0; tx.eop = 1'b1;}) $display("ERROR:DMA GENERATOR::RANDOMIZATION FAILED FOR BASIC READ WRITE SANITY TEST:PRE WRITE READ");
 		// mailbox for inter communication between classes
-		dma_cfg::gen2drv.put(tx); 	// send transaction to Driver to drive it to the DUT using mailbox put() method
-		dma_cfg::gen2ref.put(tx); 	// send transaction to reference model for generating expected values using mailbox put() method
+		dma_cfg::gen2drv.put(tx);// send transaction to Driver to drive it to the DUT using mailbox put() method
+		dma_cfg::drv2gen.get(rx);
+		$display("RESET REGISTER ")
+		rx.print();
+		end
+		
+		//WRITTING REGISTERS
+		for(int i = 0; i<`ADDRESS_LENGTH; i++)begin
+		tx = new();
+		if(!tx.randomize() with {tx.cs = 1'b0; tx.ior = 1'b1; tx.iow = 1'b0; tx.addr_lo = i; tx.data = 8'ha5; tx.dreq = 4'b0; tx.hlda = 1'b0; tx.eop = 1'b1;}) $display("ERROR:DMA GENERATOR::RANDOMIZATION FAILED FOR BASIC READ WRITE SANITY TEST: WRITE ");
+		// mailbox for inter communication between classes
+		dma_cfg::gen2drv.put(tx);// send transaction to Driver to drive it to the DUT using mailbox put() method
+		txQ.push_front(tx);
+		reg_test++;
+		end	
+		
+		//READING REGISTERS
+		for(int i = 0; i<`ADDRESS_LENGTH; i++)begin
+		tx = new();
+		if(!tx.randomize() with {tx.cs = 1'b0; tx.ior = 1'b0; tx.iow = 1'b1;  tx.addr_lo = i; tx.dreq = 4'b0; tx.hlda = 1'b0; tx.eop = 1'b1;}) $display("ERROR:DMA GENERATOR::RANDOMIZATION FAILED FOR BASIC READ WRITE SANITY TEST:POST WRITE READ");
+		// mailbox for inter communication between classes
+		dma_cfg::gen2drv.put(tx);// send transaction to Driver to drive it to the DUT using mailbox put() method
+		dma_cfg::drv2gen.get(rx);// get transaction from driver which is output of design  
+		txQ.pop_back(tx);
+		if(tx.data == rx.data)
+		reg_check_pass++;
+		else
+		reg_check_fail++;
+		end
+		
+		if(reg_test == reg_check_pass)
 		dma_cfg::num_trans++;
-		end // repeat
+		else
+		$display("ERROR:DMA GENERATOR::BASIC READ WRITE SANITY TEST:TEST FAILED");
 		endtask
-
+		
+	//random_rd_wr_test
+	//dma_request_priority_test
+	//dma_eop_directed_test
+	//dma_polarity_control_directed_test
+	//
+// REGISTER READ TASK
+//REGISTER WRITE TASK
 
 endclass
