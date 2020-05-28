e373b751037e8ed0e95f7a87016c340c3fabb2d5
Rishitosh Sawant <srishis.214@gmail.com>
2020-05-28 12:18:55 -0700

    Created scoreboard template.

---
+++ dma_sb.sv
@@ -0,0 +1,19 @@
+class dma_sb;
+  
+  //used to count the number of transactions
+  int num_trans;
+  
+  dma_transaction  tx, tx1;
+  
+  task run();
+    
+    forever begin
+      dma_cfg::mon2sb.get(tx);
+      dma_cfg::ref2sb.get(tx1);
+      
+      num_trans++;
+    end
+ 
+  endtask : run
+  
+endclass : dma_sb
