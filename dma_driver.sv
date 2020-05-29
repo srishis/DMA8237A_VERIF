2aedc4f35cdf1c09a3ab6e5f47b19875b2d3c1d0
Rishitosh Sawant <srishis.214@gmail.com>
2020-05-29 09:27:47 -0700

    Added calls to checker.

--- dma_driver.sv
+++ dma_driver.sv
@@ -80,6 +80,7 @@ class dma_driver;
 	task drive_stimulus(dma_transaction tx);
 		// enable DMA controller by asserting Chip select
 		vif.dma_cb.CS_N 	<= 0;
+		while(vif.dma_cb.EOP_N == 1) begin
 		repeat(10)@(vif.cb);
 		vif.dma_cb.DREQ 	<= tx.dreq;
 		// wait for HRQ from DMA for one cycle
@@ -88,15 +89,15 @@ class dma_driver;
 			@(vif.cb);
 		join_any
 		// check HRQ is asserted
-		//TODO: check_hrq(1);
-		assert(vif.dma_cb.HRQ == 1)
-		else $error("ERROR:DMA DRIVER:: HRQ NOT asserted!!!!");
+		check_hrq(1);
+		// assert(vif.dma_cb.HRQ == 1)
+		// else $error("ERROR:DMA DRIVER:: HRQ NOT asserted!!!!");
 		// call drive_dut to configure control registers
 		//TODO: ensure all control registers are written before moving on
-		while(vif.dma_cb.EOP_N == 1)
+		while(vif.dma_cb.EOP_N == 1) begin
 		if(vif.dma_cb.HRQ == 1) begin
 			drive_dut(tx);
-			// wait(ctrl_regs_wr_done == 1);
+			wait(ctrl_regs_wr_done == 1);
 			// Give acknowledge to DMA that CPU is handing over the bus to DMA
 			vif.dma_cb.HLDA	<= 1;
 			@(vif.cb);
@@ -107,8 +108,8 @@ class dma_driver;
 				Read TEMP_ADDR_REG[15:8] or IO Data buffer to get the high order Address[A8-A15]
 				Read TEMP_ADDR_REG[7:0] or both IO Address buffer and Output Address buffer
 				or {ADDR_L, ADDR_U} to get the low order Address[A0-A7]
-				check_address_match();
 			*/
+			check_address_match();
 			
 			// assert(virt_inf.cb.DB === test_Addr[channels[3]]) else $error("Wrong Base address asserted for the channel when ADSTB is asserted");
 			// wait for DACK from DMA for one cycle
@@ -117,56 +118,64 @@ class dma_driver;
 				@(vif.dma_cb);
 			join_any
 			//TODO: check DACK matches the sampled value
-			// check_dack(tx.dack_sampled);
-			assert(vif.dma_cb.DACK != tx.dack_sampled)
-			else $error("ERROR:DMA DRIVER:: DACK NOT asserted!!!!");
+			check_dack(tx.dack_sampled);
+			// assert(vif.dma_cb.DACK != tx.dack_sampled)
+			// else $error("ERROR:DMA DRIVER:: DACK NOT asserted!!!!");
 			if(vif.dma_cb.DACK == tx.dack_sampled) begin
-				// assert Read/Write signals
+				// check if Read/Write signals are asserted
 				if(tx.type == DMA_IO_WRITE_MEM_READ) begin
-					vif.dma_cb.IOW_N 	<= 0;
-					vif.dma_cb.MEMR_N 	<= 0;
-					vif.dma_cb.IOR_N 	<= 1;
-					vif.dma_cb.MEMW_N 	<= 1;
+					// check if IOW = 0, MEMR_N = 0, IOR = 1, MEMW_N = 0
+					check_read_write_signals(0,0,1,1);
+					/*
+					assert(vif.dma_cb.IOW_N == 0);
+					assert(vif.dma_cb.MEMR_N == 0);
+					assert(vif.dma_cb.IOR_N	== 1;
+					assert(vif.dma_cb.MEMW_N == 1;
+					*/
 				end
 				else if(tx.type == DMA_IO_READ_MEM_WRITE) begin
+					// check if IOW = 1, MEMR_N = 1, IOR = 0, MEMW_N = 0
+					check_read_write_signals(0,0,1,1);
+					/*
 					vif.dma_cb.IOR_N 	<= 0;
 					vif.dma_cb.MEMW_N 	<= 0;
 					vif.dma_cb.IOW_N 	<= 1;
-					vif.dma_cb.MEMR_N 	<= 1;	
+					vif.dma_cb.MEMR_N 	<= 1;
+					*/
 				end
-				// de-assert read signals immediately after DACK is asserted
-				vif.dma_cb.MEMR_N 	<= 1;
-				vif.dma_cb.IOR_N 	<= 1;
-				// de-assert write signals after one cycle if extended write enabled
+				// check if Read/write signals are de-asserted after DACK is asserted
+				// DUT de-asserts write signals after one cycle if extended write enabled
 				// late write enabled
 				if(tx.late_write_en == 1) begin
-					vif.dma_cb.IOW_N 	<= 1;
-					vif.dma_cb.MEMW_N 	<= 1;
+					// check if IOW = 1, MEMR_N = 1, IOR = 1, MEMW_N = 1
+					check_read_write_signals(1,1,1,1);	
 				end
 				// extended write enabled
 				else if(tx.late_write_en == 0) begin
 					@(vif.dma_cb);
-					vif.dma_cb.MEMW_N 	<= 1;
-					vif.dma_cb.IOW_N 	<= 1;
+					check_read_write_signals(1,1,1,1);
 				end
 				// de-assert DREQ
 				@(vif.dma_cb);
 				vif.dma_cb.DREQ 	<= ~tx.dreq;
 				// wait for one cycle for timeout/end of process which marks end of transfer
 				fork
-					wait(vif.dma_cb.EOP_N == 0);
+					wait({vif.dma_cb.EOP_N, vif.dma_cb.HRQ, vif.dma_cb.DACK} == {1'b0, 1'b0, ~tx.dack_sampled});
 					@(vif.dma_cb);
 				join_any
-				//TODO: check EOP is asserted after transfer
-				// check_eop(0);
-				assert(vif.dma_cb.EOP_N != 0)
-				else $error("ERROR:DMA DRIVER:: EOP_N NOT asserted!!!!");
+				//TODO: check EOP, HRQ, DACK after transfer
+				check_eop(0);
+				check_hrq(0);
+				check_dack(~tx.dack_sampled);
+				//assert(vif.dma_cb.EOP_N != 0)
+				// else $error("ERROR:DMA DRIVER:: EOP_N NOT asserted!!!!");
 			
 			end 	// end of DACK check
 			
 		end 	// end of HRQ check
 		
-			
+		end		// end of EOP while loop
+		
 	endtask : drive_stimuluss
 	
 	
