d18552b48c53187d8a6c06497baab90ab241ef26
Justin Bloom <jubloom@rita-temp.cecs.pdx.edu>
2020-05-29 23:07:55 -0700

    updated scoreboard

--- dma_scoreboard.sv
+++ dma_scoreboard.sv
@@ -10,7 +10,17 @@ class dma_scoreboard;
     forever begin
       dma_cfg::mon2sb.get(tx);
       dma_cfg::ref2sb.get(tx1);
-      
+
+      if ((tx.d == 0) && (tx1.d == 0))
+        if (!tx1.compare(tx)) begin
+	  $display("ERROR: Scoreboard: Mismatch when d is equal to 0, expected=%0p, actual=%0p", tx1, tx);
+	  dma_cfg::error_count++;
+	end
+      else if ((tx.d == 1) && (tx1.d == 1))
+	if (!tx1.compare(tx)) begin
+	  $display("ERROR: Scoreboard: Mismatch when d is equal to 1, expected=%0p, actual=%0p", tx1, tx);
+	  dma_cfg::error_count++;
+	end
       num_trans++;
     end
  
