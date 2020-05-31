9e10fa5d26742c5f306a821e2a8e762b3da0b1eb
Justin Bloom <jubloom@rita-temp.cecs.pdx.edu>
2020-05-30 20:25:35 -0700

    fixed incorrect part of scoreboard

--- dma_scoreboard.sv
+++ dma_scoreboard.sv
@@ -11,16 +11,10 @@ class dma_scoreboard;
       dma_cfg::mon2sb.get(tx);
       dma_cfg::ref2sb.get(tx1);
 
-      if ((tx.d == 0) && (tx1.d == 0))
-        if (!tx1.compare(tx)) begin
-	  $display("ERROR: Scoreboard: Mismatch when d is equal to 0, expected=%0p, actual=%0p", tx1, tx);
+      if (!tx1.compare(tx)) begin
+	  $display("ERROR: Scoreboard: Mismatch expected=%0p, actual=%0p", tx1, tx);
 	  dma_cfg::error_count++;
-	end
-      else if ((tx.d == 1) && (tx1.d == 1))
-	if (!tx1.compare(tx)) begin
-	  $display("ERROR: Scoreboard: Mismatch when d is equal to 1, expected=%0p, actual=%0p", tx1, tx);
-	  dma_cfg::error_count++;
-	end
+      end
       num_trans++;
     end
  
