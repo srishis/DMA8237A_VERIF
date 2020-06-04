38d89fd2097de37a94812f8f3b20c7fb0e1932ae
Justin Bloom <justinbloom@pdx.edu>
2020-06-04 00:57:58 -0700

    small file updates

--- dma_scoreboard.sv
+++ dma_scoreboard.sv
@@ -8,8 +8,10 @@ class dma_scoreboard;
   task run();
     
     forever begin
-      //`dma_cfg::mon2sb.get(tx);
-      dma_cfg::ref2sb.get(tx1);
+      //dma_cfg::mon2sb.get(tx);
+      //dma_cfg::ref2sb.get(tx1);
+      dma_cfg::gen2sb(tx); // for now just pass the data directly from sb to checker
+      dma_cfg::sb2ck(tx);
 
       //if (!tx1.compare(tx)) begin
 	  //$display("ERROR: Scoreboard: Mismatch expected=%0p, actual=%0p", tx1, tx);
